f306426bd0fb822a08dd29f1c1560973bf22fca1
oyamo <oyamo.xyz@gmail.com>
2022-10-24 01:28:16 +0300

    configure network util

--- network.go
+++ network.go
@@ -1,22 +1,74 @@
 package darajago
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
 )
 
-type networkRequest struct {
-	Payload interface{} 
+type networkPackage struct {
+	Payload io.Reader
 	Endpoint string
 	Method string
 	Headers map[string]string
 
 }
 
-type networkResponse struct {
-	Body string
+type networkResponse[T any] struct {
+	Body T
 	StatusCode int
 }
 
-type network interface {
-	MakeRequest(request networkRequest) (networkResponse, error)
+func newPackage(payload map[string] interface{}, endpoint string, method string, headers map[string]string) *networkPackage {
+	var payloadReader io.Reader
+	if method == http.MethodPost {
+		payloadBytes, _ := json.Marshal(payload)
+		payloadReader = bytes.NewReader(payloadBytes)
+	} else if method == http.MethodGet {
+		q := url.Values{}
+		for key, value := range payload {
+			q.Add(key, value.(string))
+		}
+		payloadReader = bytes.NewReader([]byte(q.Encode()))
+	}
+
+	return &networkPackage{
+		Payload: payloadReader,
+		Endpoint: endpoint,
+		Method: method,
+		Headers: headers,
+	}
 }
 
+func newRequest[T any](pac *networkPackage) (*networkResponse[T], error) {
+	res := &networkResponse[T]{}
+	client := &http.Client{}
+	
+	req, err := http.NewRequest(pac.Method, pac.Endpoint, nil)
+	if err != nil {
+		return res, err
+	}
+
+	for key, value := range pac.Headers {
+		req.Header.Add(key, value)
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return res, err
+	}
+
+	defer resp.Body.Close()
+
+	res.StatusCode = resp.StatusCode
+
+	if err := json.NewDecoder(resp.Body).Decode(&res.Body); err != nil {
+		return res, err
+	}
+
+	return res, nil
+
+}
