89e597e89c329e56a6dfed581b617dd235d2921c
oyamo <oyamo.xyz@gmail.com>
2023-01-04 17:23:43 +0300

    add model

--- consts.go
+++ consts.go
@@ -7,9 +7,9 @@ const (
 	endpointReversal                  = "/mpesa/reversal/v1/request"                       // The endpoint for reversing a transaction
 	endpointB2BReq                    = "/mpesa/b2b/v1/paymentrequest"                     // The endpoint for making a B2B payment request
 	endpointTxStatus                  = "/mpesa/transactionstatus/v1/query"                // The endpoint for querying a transaction status
-	endpointSimulatePmtC2B            = "/mpesa/c2b/v1/simulate"                           // The endpoint for simulating a C2B transaction
+	endpointSimulatePmtC2B            = "/mpesa/c2b/v1/simulate"                           // The endpoint for simulating a C2BSimulation transaction
 	endpointQueryLipanaMpesa          = "/mpesa/stkpushquery/v1/query"                     // The endpoint for querying a Lipa Na Mpesa transaction
 	endpointB2CPmtReq                 = "/mpesa/b2c/v1/paymentrequest"                     // The endpoint for making a B2C payment request
 	endpointAccountBalance            = "/mpesa/accountbalance/v1/query"                   // The endpoint for querying an M-Pesa account balance
-	endpointRegisterConfirmValidation = "/mpesa/c2b/v1/registerurl"                        // The endpoint for confirming a C2B transaction
+	endpointRegisterConfirmValidation = "/mpesa/c2b/v1/registerurl"                        // The endpoint for confirming a C2BSimulation transaction
 )
--- model.go
+++ model.go
@@ -47,3 +47,68 @@ type RegisterURLConfig struct {
 	ConfirmationURL string `json:"ConfirmationURL"`
 	ValidationURL   string `json:"ValidationURL"`
 }
+
+type TransactionStatus struct {
+	Initiator          string `json:"Initiator"`
+	SecurityCredential string `json:"SecurityCredential"`
+	CommandID          string `json:"CommandID"`
+	TransactionID      string `json:"TransactionID"`
+	PartyA             string `json:"PartyA"`
+	IdentifierType     string `json:"IdentifierType"`
+	ResultURL          string `json:"ResultURL"`
+	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
+	Remarks            string `json:"Remarks"`
+	Occasion           string `json:"Occasion"`
+}
+
+// C2BSimulation Simulate paying bills online
+type C2BSimulation struct {
+	ShortCode     string `json:"ShortCode"`
+	CommandID     string `json:"CommandID"`
+	Amount        string `json:"Amount"`
+	Msisdn        string `json:"Msisdn"`
+	BillRefNumber string `json:"BillRefNumber"`
+}
+
+// MerchantStatus is used to check the status of a transaction on Lipa Na M-Pesa Online Payment
+type MerchantStatus struct {
+	BusinessShortCode string `json:"BusinessShortCode"`
+	Password          string `json:"Password"`
+	Timestamp         string `json:"Timestamp"`
+	CheckoutRequestID string `json:"CheckoutRequestID"`
+}
+
+// InitiateLipaNaMpesa is used to initiate a transaction on Lipa Na M-Pesa Online Payment
+type InitiateLipaNaMpesa struct {
+	BusinessShortCode string `json:"BusinessShortCode"`
+	Password          string `json:"Password"`
+	Timestamp         string `json:"Timestamp"`
+	TransactionType   string `json:"TransactionType"`
+	Amount            string `json:"Amount"`
+	PartyA            string `json:"PartyA"`
+	PartyB            string `json:"PartyB"`
+	PhoneNumber       string `json:"PhoneNumber"`
+	CallBackURL       string `json:"CallBackURL"`
+	AccountReference  string `json:"AccountReference"`
+	TransactionDesc   string `json:"TransactionDesc"`
+}
+
+// BalanceQuery is used to query the balance of an M-Pesa account
+type BalanceQuery struct {
+	Initiator          string `json:"Initiator"`
+	SecurityCredential string `json:"SecurityCredential"`
+	CommandID          string `json:"CommandID"`
+	PartyA             string `json:"PartyA"`
+	IdentifierType     string `json:"IdentifierType"`
+	Remarks            string `json:"Remarks"`
+	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
+	ResultURL          string `json:"ResultURL"`
+}
+
+// C2BURLRegistration is used to register the confirmation and validation URLs
+type C2BURLRegistration struct {
+	ShortCode       string `json:"ShortCode"`
+	ResponseType    string `json:"ResponseType"`
+	ConfirmationURL string `json:"ConfirmationURL"`
+	ValidationURL   string `json:"ValidationURL"`
+}
