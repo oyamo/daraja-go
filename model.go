b9874fc8655b999df8065fc015ffe7d9c167a205
oyamo <oyamo.xyz@gmail.com>
2023-01-04 20:47:41 +0300

    migrate: models

--- model.go
+++ model.go
@@ -22,38 +22,6 @@ type B2CConfig struct {
 	ResultURL              string `json:"ResultURL"`
 }
 
-type B2CResponse struct {
-	OriginatorConversationID string `json:"OriginatorConversationID"`
-	ConversationID           string `json:"ConversationID"`
-	ResponseDescription      string `json:"ResponseDescription"`
-}
-
-type ReversalConfig struct {
-	Initiator              string `json:"Initiator"`
-	SecurityCredential     string `json:"SecurityCredential"`
-	CommandID              string `json:"CommandID"`
-	TransactionID          string `json:"TransactionID"`
-	Amount                 string `json:"Amount"`
-	ReceiverParty          string `json:"ReceiverParty"`
-	RecieverIdentifierType string `json:"RecieverIdentifierType"`
-	ResultURL              string `json:"ResultURL"`
-	QueueTimeOutURL        string `json:"QueueTimeOutURL"`
-	Remarks                string `json:"Remarks"`
-	Occasion               string `json:"Occasion"`
-}
-
-// ReversalResponse represents a response from the Mpesa Reversal.
-type ReversalResponse struct {
-	// OriginatorConversationID is the unique request ID for tracking a transaction.
-	OriginatorConversationID string `json:"OriginatorConversationID"`
-
-	// ConversationID is the unique request ID returned by mpesa for each request made.
-	ConversationID string `json:"ConversationID"`
-
-	// ResponseDescription is the response description message.
-	ResponseDescription string `json:"ResponseDescription"`
-}
-
 type RegisterURLConfig struct {
 	ShortCode       string `json:"ShortCode"`
 	ResponseType    string `json:"ResponseType"`
@@ -72,15 +40,6 @@ type TransactionStatus struct {
 	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
 	Remarks            string `json:"Remarks"`
 	Occasion           string `json:"Occasion"`
-}
-
-// C2BSimulation Simulate paying bills online
-type C2BSimulation struct {
-	ShortCode     string `json:"ShortCode"`
-	CommandID     string `json:"CommandID"`
-	Amount        string `json:"Amount"`
-	Msisdn        string `json:"Msisdn"`
-	BillRefNumber string `json:"BillRefNumber"`
 }
 
 // MerchantStatus is used to check the status of a transaction on Lipa Na M-Pesa Online Payment
