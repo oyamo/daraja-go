6f3f066e93fd5bac53fd75553fd6b83a1872338e
oyamo <oyamo.xyz@gmail.com>
2023-01-04 20:48:19 +0300

    feat: reversal of money

--- reversal.go
+++ reversal.go
@@ -1,5 +1,31 @@
 package darajago
 
+type ReversalConfig struct {
+	Initiator              string `json:"Initiator"`
+	SecurityCredential     string `json:"SecurityCredential"`
+	CommandID              string `json:"CommandID"`
+	TransactionID          string `json:"TransactionID"`
+	Amount                 string `json:"Amount"`
+	ReceiverParty          string `json:"ReceiverParty"`
+	RecieverIdentifierType string `json:"RecieverIdentifierType"`
+	ResultURL              string `json:"ResultURL"`
+	QueueTimeOutURL        string `json:"QueueTimeOutURL"`
+	Remarks                string `json:"Remarks"`
+	Occasion               string `json:"Occasion"`
+}
+
+// ReversalResponse represents a response from the Mpesa Reversal.
+type ReversalResponse struct {
+	// OriginatorConversationID is the unique request ID for tracking a transaction.
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+
+	// ConversationID is the unique request ID returned by mpesa for each request made.
+	ConversationID string `json:"ConversationID"`
+
+	// ResponseDescription is the response description message.
+	ResponseDescription string `json:"ResponseDescription"`
+}
+
 func (d *DarajaApi) ReverseTransaction(transation ReversalConfig) (*ReversalResponse, *ErrorResponse) {
 	transation.CommandID = "TransactionReversal"
 	// marshal the struct into a map
