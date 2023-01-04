0ca8a0a3bd9da5d5e090a26eedc5c4bfe0c1a59c
oyamo <oyamo.xyz@gmail.com>
2023-01-04 19:24:04 +0300

    feat: lipa na mpesa

--- stkpush.go
+++ stkpush.go
@@ -1,3 +1,15 @@
 package darajago
 
-// initiate an stk push
+// LipaNaMpesaOnline is a function that initiates a Lipa Na Mpesa Online payment.
+// It takes in a LipaNaMpesa struct representing the payment configuration,
+// and returns a LipaNaMpesaResponse struct representing the response from the Lipa Na Mpesa API,
+// or an ErrorResponse struct representing an error that occurred during the request.
+func (d *DarajaApi) LipaNaMpesaOnline(mpesaConfig LipaNaMpesa) (*LipaNaMpesaResponse, *ErrorResponse) {
+	// marshal the struct into a map
+	payload := struct2Map(mpesaConfig)
+	secureResponse, err := performSecurePostRequest[LipaNaMpesaResponse](payload, endpointLipaNaMpesa, d)
+	if err != nil {
+		return nil, err
+	}
+	return &secureResponse.Body, nil
+}
