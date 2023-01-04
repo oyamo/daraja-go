826b07aa37191ba25f33d231fde7457e2f52a3f1
oyamo <oyamo.xyz@gmail.com>
2023-01-04 10:31:29 +0300

    feat: add authorisation function

--- authorisation.go
+++ authorisation.go
@@ -1,26 +1,35 @@
 package darajago
 
+import (
+	"encoding/base64"
+	"net/http"
+)
 
 const (
 	auth_endpoint = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 )
 
-type authPayload struct {
-	Authorization string `json:"authorization"` // Base64 encoded string of the consumer key and secret
-	GrantType     string `json:"grant_type"`    // client_credentials
-}
-
 type authResponse struct {
-	AccessToken string `json:"access_token"`   // The access token to be used in subsequent API calls
-	ExpiresIn   string `json:"expires_in"`     // The number of seconds before the access token expires
+	AccessToken string `json:"access_token"` // The access token to be used in subsequent API calls
+	ExpiresIn   string `json:"expires_in"`   // The number of seconds before the access token expires
 }
-
 
 type Authorization struct {
 	authResponse
 }
 
+func NewAuthorization(consumerKey, consumerSecret string) (*Authorization, error) {
+	auth := &Authorization{}
+	authHeader := map[string]string{
+		"Authorisation": base64.StdEncoding.EncodeToString([]byte(consumerKey + ":" + consumerSecret)),
+	}
 
-func NewAuthorization(authorisation string, grantType string) (*Authorization, error){
-	
+	netPackage := newPackage(nil, auth_endpoint, http.MethodGet, authHeader)
+	authResponse, err := newRequest[Authorization](netPackage)
+	if err != nil {
+		return nil, err
+	}
+
+	auth = &authResponse.Body
+	return auth, nil
 }
