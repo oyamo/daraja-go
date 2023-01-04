d17c7ec67e6290657e0cc2f19c5be9f22ee7070f
oyamo <oyamo.xyz@gmail.com>
2023-01-04 19:32:10 +0300

    move: lipanampesa & lipanampesa structs to stkpush.go

--- model.go
+++ model.go
@@ -91,39 +91,6 @@ type MerchantStatus struct {
 	CheckoutRequestID string `json:"CheckoutRequestID"`
 }
 
-// LipaNaMpesa is used to initiate a transaction on Lipa Na M-Pesa Online Payment
-type LipaNaMpesa struct {
-	BusinessShortCode string `json:"BusinessShortCode"`
-	Password          string `json:"Password"`
-	Timestamp         string `json:"Timestamp"`
-	TransactionType   string `json:"TransactionType"`
-	Amount            string `json:"Amount"`
-	PartyA            string `json:"PartyA"`
-	PartyB            string `json:"PartyB"`
-	PhoneNumber       string `json:"PhoneNumber"`
-	CallBackURL       string `json:"CallBackURL"`
-	AccountReference  string `json:"AccountReference"`
-	TransactionDesc   string `json:"TransactionDesc"`
-}
-
-// LipaNaMpesaResponse represents a response from the Lipa Na Mpesa API.
-type LipaNaMpesaResponse struct {
-	// MerchantRequestID is the unique request ID for tracking a transaction.
-	MerchantRequestID string `json:"MerchantRequestID"`
-
-	// CheckoutRequestID is the unique request ID for the checkout process.
-	CheckoutRequestID string `json:"CheckoutRequestID"`
-
-	// ResponseCode is the response code for the request.
-	ResponseCode string `json:"ResponseCode"`
-
-	// ResponseDescription is a description of the response.
-	ResponseDescription string `json:"ResponseDescription"`
-
-	// CustomerMessage is a message for the customer.
-	CustomerMessage string `json:"CustomerMessage"`
-}
-
 // BalanceQuery is used to query the balance of an M-Pesa account
 type BalanceQuery struct {
 	Initiator          string `json:"Initiator"`
--- stkpush.go
+++ stkpush.go
@@ -1,5 +1,38 @@
 package darajago
 
+// LipaNaMpesa is used to initiate a transaction on Lipa Na M-Pesa Online Payment
+type LipaNaMpesa struct {
+	BusinessShortCode string `json:"BusinessShortCode"`
+	Password          string `json:"Password"`
+	Timestamp         string `json:"Timestamp"`
+	TransactionType   string `json:"TransactionType"`
+	Amount            string `json:"Amount"`
+	PartyA            string `json:"PartyA"`
+	PartyB            string `json:"PartyB"`
+	PhoneNumber       string `json:"PhoneNumber"`
+	CallBackURL       string `json:"CallBackURL"`
+	AccountReference  string `json:"AccountReference"`
+	TransactionDesc   string `json:"TransactionDesc"`
+}
+
+// LipaNaMpesaResponse represents a response from the Lipa Na Mpesa API.
+type LipaNaMpesaResponse struct {
+	// MerchantRequestID is the unique request ID for tracking a transaction.
+	MerchantRequestID string `json:"MerchantRequestID"`
+
+	// CheckoutRequestID is the unique request ID for the checkout process.
+	CheckoutRequestID string `json:"CheckoutRequestID"`
+
+	// ResponseCode is the response code for the request.
+	ResponseCode string `json:"ResponseCode"`
+
+	// ResponseDescription is a description of the response.
+	ResponseDescription string `json:"ResponseDescription"`
+
+	// CustomerMessage is a message for the customer.
+	CustomerMessage string `json:"CustomerMessage"`
+}
+
 // LipaNaMpesaOnline is a function that initiates a Lipa Na Mpesa Online payment.
 // It takes in a LipaNaMpesa struct representing the payment configuration,
 // and returns a LipaNaMpesaResponse struct representing the response from the Lipa Na Mpesa API,
